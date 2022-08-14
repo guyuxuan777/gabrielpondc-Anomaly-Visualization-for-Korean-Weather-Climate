b1f7b2399f2971addc772df2aca69ac6d3323e12
gabrielpondc <gabrielpondc@yahoo.com>
2022-08-14 15:46:06 +0800

    读取优化

--- qqq13.php
+++ qqq13.php
@@ -24,15 +24,14 @@ $db_selected=@mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could 
   // mysqli_query("SET NAMES utf8");  
   // mysqli_query()
   $sql = "SELECT DATE_FORMAT(weather.date,'%Y-%m-%d') time FROM test,weather where test.KID=weather.KID and weather.date >='$start_time' AND weather.date < '$end_time' group by time";
-  $sql1 = "SELECT weather.dangdiqiya FROM test,weather where test.KID=weather.KID and weather.date >='$start_time' AND weather.date < '$end_time'";
+  $sql1 = "SELECT test.city,weather.dangdiqiya FROM test,weather where test.KID=weather.KID and test.city='$name' and weather.date >='$start_time' AND weather.date < '$end_time' group by time";
   $result = mysqli_query($dbc,$sql); 
-  $result1 = mysqli_query($dbc,$sql1); 
+  $result = mysqli_query($dbc,$sql); 
   //定义变量json存储值
   $data="";
   $array= array();
   $array=$res;
   $time=array();
-  $time1=array();
   class emp{
     public $time;
     public $value;
@@ -45,31 +44,20 @@ $db_selected=@mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could 
     //数组赋值
    
   }
-  while ($row = mysqli_fetch_row($result1))
-  { 
-    array_push($time1,$row);  
-    
-    //数组赋值
-   
-  }
-  $sum=0;
   for ($i=0;$i<count($time);$i++){
     $array1[] = [$time[$i][0],floatval($array[$i])];
   }
-
-  $n=count($time1)/24
-  //$arrayavg=array();
-  //for ($i=0;$i<count($time1)/24;$i++){
-    //$sum=0;
-    //for ($j=0;$j<24;$j++){
-       // $sum=$sum+$time1[$j+($i*24)];
-    //}
-    //array_push($arrayavg,$sum/24);
-
- //}
-
+  $array2= array();
+  $arrayavg= array();
+  for ($i=0;$i<count($arr)/24;$i++){
+    $sum=0;
+    for ($j=0;$j<24;$j++){
+        $sum=$sum+$arr[$j+($i*24)]
+    }
+    array_push($arrayavg,$sum/24)
+  }
  
 $data = json_encode($array1);
-print_r ($n);
+echo $data;
 
 ?>
